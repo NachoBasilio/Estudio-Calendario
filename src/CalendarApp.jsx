5161c7e71d5ddb499f7560e1b330fc41223a1802
IgnaDev <arcortoon@gmail.com>
2023-08-30 15:58:51 -0300

    first commit

---
+++ src/CalendarApp.jsx
@@ -0,0 +1,6 @@
+
+export default function CalendarApp() {
+  return (
+    <h1>calendarApp</h1>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import CalendarApp from './CalendarApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CalendarApp />
+  </React.StrictMode>,
+)
